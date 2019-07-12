f8874c83b1b79ff6d1e15b9eddc1b39c9e2611bc
雨落随风 <bshsf@qq.com>
2019-07-13 01:17:13 +0800

    PointId是从-3开始到正向的无限大（假定支持无限指头触控），所以修改字段 fingerID 预设值为 负的无限大

--- Assets/Joystick/Joystick.cs
+++ Assets/Joystick/Joystick.cs
@@ -1,34 +1,30 @@
-﻿using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.Events;
-
-namespace zFrame.UI
+﻿namespace zFrame.UI
 {
-    /// <summary>
-    /// 简易而不简单的摇杆组件
-    /// </summary>
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+    using UnityEngine.Events;
     public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
     {
         public float maxRadius = 100; //Handle 移动最大半径
         [SerializeField, EnumFlags]
-        private Direction activatedAxis = (Direction)(-1);
+        Direction activatedAxis = (Direction)(-1); //选择激活的轴向
         [SerializeField]
-        private bool showDirection = true;
-        public JoystickEvent OnValueChanged = new JoystickEvent(); //事件
-        public bool IsDraging { get{return fingerId!=-1;} }
-        
+        bool showDirection = true;
+        public JoystickEvent OnValueChanged = new JoystickEvent(); //Update驱动的事件
+        public bool IsDraging { get { return fingerId != int.MinValue; } } //可供外部断言当前摇杆状态
+
         private RectTransform backGround, handle, direction; //摇杆背景、摇杆手柄、方向指引
-        private Vector2 joysticValue = Vector2.zero;
+        private Vector2 joystickValue = Vector2.zero;
         private Vector3 backGroundOriginLocalPostion;
-        private int fingerId=-1;
-        [System.Serializable] public class JoystickEvent : UnityEvent<Vector2> { }
+        private int fingerId = int.MinValue; //当前触发摇杆的 pointerId ，预设一个永远无法企及的值
+        [System.Serializable]
+        public class JoystickEvent : UnityEvent<Vector2> { }
         [System.Flags]
         public enum Direction
         {
             Horizontal = 1 << 0,
             Vertical = 1 << 1
         }
-
         private void Awake()
         {
             backGround = transform.Find("BackGround") as RectTransform;
@@ -40,39 +36,31 @@ namespace zFrame.UI
 
         void Update()
         {
-            if (IsDraging) //摇杆拖拽进行时驱动事件
-            {
-                joysticValue.x = handle.anchoredPosition.x / maxRadius;
-                joysticValue.y = handle.anchoredPosition.y / maxRadius;
-                OnValueChanged.Invoke(joysticValue);
-            }
+            if (!IsDraging) return;//仅当摇杆拖拽时驱动事件
+            joystickValue.x = handle.anchoredPosition.x / maxRadius;
+            joystickValue.y = handle.anchoredPosition.y / maxRadius;
+            OnValueChanged.Invoke(joystickValue);
         }
 
-        /// <summary>
-        /// 显示摇杆底盘
-        /// </summary>
-        /// <param name="eventData"></param>
+        // 摇杆被触发，初始化摇杆
         void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
         {
-            if(IsDraging)return;
+            if (IsDraging) return;
             fingerId = eventData.pointerId;
             Vector3 backGroundPos = new Vector3() // As it is too long for trinocular operation so I create Vector3 like this.
             {
-               x = eventData.position.x,
-               y = eventData.position.y,
-               z = (null == eventData.pressEventCamera) ? backGround.position.z :
+                x = eventData.position.x,
+                y = eventData.position.y,
+                z = (null == eventData.pressEventCamera) ? backGround.position.z :
                  eventData.pressEventCamera.WorldToScreenPoint(backGround.position).z //无奈，这个坐标转换不得不做啊,就算来来回回的折腾。
             };
             backGround.position = (null == eventData.pressEventCamera) ? backGroundPos : eventData.pressEventCamera.ScreenToWorldPoint(backGroundPos);
         }
 
-        /// <summary>
-        /// 当鼠标拖拽时
-        /// </summary>
-        /// <param name="eventData"></param>
+        // 当鼠标拖拽时
         void IDragHandler.OnDrag(PointerEventData eventData)
         {
-            if(fingerId!=eventData.pointerId)return;
+            if (fingerId != eventData.pointerId) return;
             Vector2 backGroundPos = (null == eventData.pressEventCamera) ?
                 backGround.position : eventData.pressEventCamera.WorldToScreenPoint(backGround.position);
             Vector2 direction = eventData.position - backGroundPos; //得到方位盘中心指向光标的向量
@@ -86,9 +74,7 @@ namespace zFrame.UI
             UpdateDirectionArrow(localPosition);
         }
 
-        /// <summary>
-        /// 更新指向器的朝向
-        /// </summary>
+        // 更新指向器的朝向
         private void UpdateDirectionArrow(Vector2 position)
         {
             if (showDirection && position.magnitude != 0)
@@ -98,18 +84,14 @@ namespace zFrame.UI
             }
         }
 
-        /// <summary>
-        /// 当鼠标停止拖拽时
-        /// </summary>
-        /// <param name="eventData"></param>
+        // 当鼠标停止拖拽时,重置数据
         void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
         {
-            if(fingerId!=eventData.pointerId)return;
-            fingerId=-1;
+            if (fingerId != eventData.pointerId) return;
+            fingerId = int.MinValue;
             direction.gameObject.SetActive(false);
-            backGround.localPosition = backGroundOriginLocalPostion ;
+            backGround.localPosition = backGroundOriginLocalPostion;
             handle.localPosition = Vector3.zero;
-            
         }
     }
 }
