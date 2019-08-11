e309b0cdfc9e7093b3efd50168385a5d9b039447
雨落随风 <bshsf@qq.com>
2019-08-11 13:20:15 +0800

    test characterController move

---
+++ Assets/Joystick/Example/CharacterControllerMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using zFrame.UI;
+
+public class CharacterControllerMove : MonoBehaviour
+{
+    [SerializeField] Joystick joystick;
+    [SerializeField] Joystick joystick2;
+    public float speed = 5;
+    CharacterController controller;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        joystick.OnValueChanged.AddListener(v =>
+        {
+            if (v.magnitude != 0)
+            {
+                Vector3 direction = transform.TransformDirection(new Vector3(v.x, 0, v.y));
+                controller.Move(direction * speed * Time.deltaTime);
+            }
+        });
+        joystick2.OnValueChanged.AddListener(v =>
+        {
+            if (v.magnitude != 0)
+            {
+                transform.rotation = Quaternion.LookRotation(new Vector3(v.x, 0, v.y));
+            }
+        });
+    }
+}
--- Assets/Joystick/Example/CharacterControllerMove0.cs
+++ Assets/Joystick/Example/CharacterControllerMove0.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using zFrame.UI;
 
-public class Mover : MonoBehaviour
+public class CharacterControllerMove0 : MonoBehaviour
 {
     [SerializeField] Joystick joystick;
     public float speed = 5;
+    CharacterController controller;
     void Start()
     {
+        controller = GetComponent<CharacterController>();
+
         joystick.OnValueChanged.AddListener(v =>
         {
             if (v.magnitude != 0)
             {
-                transform.Translate(v.x * speed*Time.deltaTime, 0, v.y * speed*Time.deltaTime, Space.World);
+                Vector3 direction = new Vector3(v.x, 0, v.y);
+                controller.Move(direction * speed * Time.deltaTime);
                 transform.rotation = Quaternion.LookRotation(new Vector3(v.x, 0, v.y));
             }
         });
